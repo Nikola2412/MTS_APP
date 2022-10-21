2e3d812cea4a08adb1d61449e1dbbd053d5c8a47
MilosPolo <tunepologame@gmail.com>
2022-10-21 11:57:49 +0200

    pfp, banner, ostavljena samo jedna ikonica za bottom bar button engage.png

--- app/src/main/java/com/example/mts_app/MojProfil.java
+++ app/src/main/java/com/example/mts_app/MojProfil.java
@@ -7,13 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MojProfil extends Fragment {
 
+    ImageView pfp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_moj_profil, container, false);
+
+
     }
 }
